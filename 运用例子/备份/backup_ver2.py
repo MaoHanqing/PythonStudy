cbffe61ce3094c657dce276f5e2d4ea79ac655b6
MaoHanqing <243520104@qq.com>
2018-01-10 18:15:25 +0800

    压缩第二版

---
+++ 运用例子/备份/backup_ver2.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+sources  = ['/Users/maohanqing/Desktop/UI']
+target_dir = '/Users/maohanqing/Desktop'
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+now = time.strftime('%H%M%S')
+
+target = today + os.sep + now + '.zip'
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory',today)
+
+zip_command = 'zip -r {} {}'.format(target,' '.join(sources))
+
+print( 'Zip command is:')
+print(zip_command)
+print('Running:')
+if os.system(zip_command) == 0:
+    print('Successful backup to',target)
+else:
+    print('Backup FAILED')
