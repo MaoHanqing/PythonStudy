b1d051608e2f648f007b371420b7882bcf4c0e2c
MaoHanqing <243520104@qq.com>
2018-01-17 21:10:03 +0800

    类型转换

---
+++ DataTransform.py
@@ -0,0 +1,14 @@
+#字符串转int
+a =  int('123')
+#浮点型转int 直接取整
+a = int(123.3)
+#字符串转float
+f = float('123.3')
+#整醒转字符串
+s = str(12)
+#浮点型转字符串
+s = str(12.3)
+#整型转bool
+b = bool(1)
+#只有大于等于1数为true 其它均为false
+b = bool('')
