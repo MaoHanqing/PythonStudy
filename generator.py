ab34cc8d29f4457d123ee6db3423476f85ff2178
MaoHanqing <243520104@qq.com>
2018-01-22 16:25:38 +0800

    生成器

--- generator.py
+++ generator.py
@@ -7,3 +7,16 @@ next(l)
 #一般用for循环，generator是可迭代的对象
 for n in l:
     print(n)
+#定义generator的另一种方法时如果函数关键词中包含yield关键字
+#那么这个函数就不再时一个普通函数而是一个generator
+#generator和函数执行流程不一样，函数遇到return就返回
+#generator函数，在每次调用next时候执行，
+#遇到yield语句返回，再次执行时候从上次返回的yield语句处继续执行
+def fib(max):
+    n, a ,b = 0, 0 ,1
+    while n < max:
+        yield b
+        a,b = b , a+b
+        n = n+1
+    return 'done'
+
