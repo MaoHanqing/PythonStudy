e3b98538ccc7d9f90700e5b5d25054515d9943be
MaoHanqing <243520104@qq.com>
2018-01-22 19:57:52 +0800

    函数

--- function.py
+++ function.py
@@ -28,4 +28,16 @@ def key_func(a,b = 10,c = 20):
 key_func(3)# 包含默认值函数
 key_func(3,4)#如果不指定参数 将会按顺序依次赋值
 
-
+#__xx__这样的变量是特殊变量，可以被直接引用，但是有特殊用途，比如
+#__author__ ,__name__就是特殊变量
+#_xx和__xxx这样的函数或变量就是非公开的不应该被直接引用
+#python没有办法可以完全限制private变量和函数访问
+def _private_1(name):
+    return 'Hello,%s' % name
+def _private_2(name):
+    return 'Hi,%s' % name
+def greeting(name):
+    if len(name)>3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
